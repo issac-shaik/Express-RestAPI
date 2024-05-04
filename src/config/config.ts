131dc1e8d56c46343013a0d6675d5511fad14315
Issac Shaik <issac.shaik@gmail.com>
2024-05-04 18:02:31 +0530

    add database setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ conf();
 //underscore for keeping the data private (best practice)
 const _config = {
   PORT: process.env.PORT,
+  databaseUrl: process.env.MONGO_CONNECTION_STRING,
 };
 
 export const config = Object.freeze(_config);
---
+++ src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to DB successfully");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.log("Error in connecting to database.", err);
+    });
+    await mongoose.connect(config.databaseUrl as string);
+  } catch (err) {
+    console.log("Failed to connect to database", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
