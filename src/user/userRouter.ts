8a7edb9450973b55c0c9297bc160f0b08b6d0609
Issac Shaik <issac.shaik@gmail.com>
2024-05-06 10:11:32 +0530

    add user router

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -3,7 +3,10 @@ import express from "express";
 const userRouter = express.Router();
 
 userRouter.post("/register", (req, res) => {
-  res.json({ message: "User registered" });
+  res.json({
+    message: "User registered",
+    title: "Welcome, user!",
+  });
 });
 
 export default userRouter;
